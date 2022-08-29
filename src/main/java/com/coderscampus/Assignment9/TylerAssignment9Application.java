542b88e9b357dab5c928e3c14819ebf9c10b9b1e
Tyler Miceli <tymiceli24@gmail.com>
2022-08-29 12:38:13 -0400

    Initial commit

---
+++ src/main/java/com/coderscampus/Assignment9/TylerAssignment9Application.java
@@ -0,0 +1,13 @@
+package com.coderscampus.Assignment9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TylerAssignment9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TylerAssignment9Application.class, args);
+	}
+
+}
